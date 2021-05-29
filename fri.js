573007060b883c83fd66a1d211b5eeaa47cffc25
avinash-2000 <71930072+avinash-2000@users.noreply.github.com>
2021-05-29 12:54:29 +0530

    Add files via upload

---
+++ fri.js
@@ -0,0 +1,128 @@
+/*
+		function clock() {
+            window. date = new Date();
+            window. hrs = date.getHours();
+            window.mins = date.getMinutes();
+            window. secs = date.getSeconds();
+            hrs = hrs < 10 ? `0${hrs}` : hrs;
+            mins = mins < 10 ? `0${mins}` : mins;
+            secs = secs < 10 ? `0${secs}` : secs;
+          
+            let time = `${hrs}:${mins}:${secs}`;
+            setInterval(clock, 1000);
+            document.getElementById("clock").innerText = time;
+      
+          }
+          
+          clock();
+          
+          function add(){
+          let a=document.getElementById("a").value;
+          let b=document.getElementById("b").value;
+          window. c=document.getElementById("c").value;
+          window.d=document.getElementById("d").value;
+          setInterval(add, 1000);
+          console.log(a,b,c,d);
+          if(hrs==a && mins==b)
+          {
+              console.log(a,b,c);
+              ale();
+          }
+          
+      
+          }
+          
+          function ale()
+          {
+              let z="would like to join"+d+ "class?"
+          var ask = window.confirm(z);
+          if (ask) {
+               window.open(c, '_blank');
+              }	
+          }
+          function add1(){
+          let a1=document.getElementById("a1").value;
+          let b1=document.getElementById("b1").value;
+          window. c1=document.getElementById("c1").value;
+          window.d1=document.getElementById("d1").value;
+          setInterval(add1, 1000);
+          console.log(a1,b1,c1,d1);
+          if(hrs==a1 && mins==b1)
+          {   console.log(a1,b1,c1);
+            ale1();
+          }
+          }
+      
+          
+          function ale1()
+          {
+              let z1="would like to join"+d1+"class?";
+          var ask1 = window.confirm(z1);
+          if (ask1) {
+            window.open(c1, '_blank');
+              } 
+          }
+          function add2(){
+          let a2=document.getElementById("a2").value;
+          let b2=document.getElementById("b2").value;
+          window. c2=document.getElementById("c2").value;
+          setInterval(add2, 1000);
+          
+          if(hrs==a2 && mins==b2)
+          {
+            ale2();
+          }
+          }
+      
+          
+          function ale2()
+          {
+         let z2="would like to join"+d2+"class?";
+          var ask2 = window.confirm(z2);
+          if (ask2) {
+            window.open(c2, '_blank');
+              } 
+          }
+          function add3(){
+          let a3=document.getElementById("a3").value;
+          let b3=document.getElementById("b3").value;
+          window. c3=document.getElementById("c3").value;
+          setInterval(add3, 1000);
+          
+          if(hrs==a3 && mins==b3)
+          {
+            ale3();
+          }
+          }
+      
+          
+          function ale3()
+          {
+          let z3="would like to join"+d3+"class?";
+          var ask3 = window.confirm(z3);
+          if (ask3) {
+            window.open(c3, '_blank');
+              } 
+          }
+          function add4(){
+          let a4=document.getElementById("a4").value;
+          let b4=document.getElementById("b4").value;
+          window. c4=document.getElementById("c4").value;
+          setInterval(add4, 1000);
+          
+          if(hrs==a4 && mins==b4)
+          {
+            ale4();
+          }
+          }
+      
+          
+          function ale4()
+          {
+          let z4="would like to join"+d4+"class?";
+          var ask4 = window.confirm(z4);
+          if (ask4) {
+            window.open(c4, '_blank');
+              } 
+          }
+          */
